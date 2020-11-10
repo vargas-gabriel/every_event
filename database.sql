347d41b68ad92a389080954cf0e9df2d608c336e
Gabriel Vargas <vargasgabriel@protonmail.com>
2020-11-10 13:35:32 -0600

    created new input fields in register form

--- database.sql
+++ database.sql
@@ -1,10 +1,46 @@
-
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL, -- becomes username?
+    password VARCHAR(1000) NOT NULL,
+    image BYTEA, -- assumes upload
+    auth_level VARCHAR(255), -- future use, do we want a default?
+    linkedin_account VARCHAR(512), -- what will this be?
+    linkedin_oauth VARCHAR(1000) -- will be inserted after token received
+);
+CREATE TABLE event (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    acronym VARCHAR(255),
+    event_image BYTEA,
+    type INTEGER NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    website VARCHAR(512),
+    registration_link VARCHAR(512),
+    linkedin_account VARCHAR(512),
+    hastag TEXT,
+    linkedin_oauth VARCHAR(1000) -- will be inserted after token received
+);
+CREATE TABLE user_event (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES "user", -- reserved keyword, s/b inside double quotes
+    event_id INTEGER REFERENCES event
+);
+CREATE TABLE phase (
+    id SERIAL PRIMARY KEY,
+    event_id INTEGER REFERENCES event,
+    name VARCHAR(255) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL
+);
+CREATE TABLE post (
+    id SERIAL PRIMARY KEY,
+    phase_id INTEGER REFERENCES phase,
+    send_date DATE NOT NULL,
+    send_time TIME NOT NULL,
+    post_text TEXT NOT NULL,
+    image BYTEA,
+    response_id VARCHAR(1000) -- will be inserted after post to Linkedin
 );
--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,70 +1,123 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import mapStoreToProps from '../../redux/mapStoreToProps';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import mapStoreToProps from "../../redux/mapStoreToProps";
 
 class RegisterForm extends Component {
-  state = {
-    username: '',
-    password: '',
-  };
+	state = {
+		firstname: "",
+		lastname: "",
+		email: "",
+		password: "",
+		verifypassword: "",
+	};
 
-  registerUser = (event) => {
-    event.preventDefault();
+	registerUser = (event) => {
+		event.preventDefault();
 
-    this.props.dispatch({
-      type: 'REGISTER',
-      payload: {
-        username: this.state.username,
-        password: this.state.password,
-      },
-    });
-  }; // end registerUser
+		this.props.dispatch({
+			type: "REGISTER",
+			payload: {
+				username: this.state.username,
+				password: this.state.password,
+			},
+		});
+	}; // end registerUser
 
-  handleInputChangeFor = (propertyName) => (event) => {
-    this.setState({
-      [propertyName]: event.target.value,
-    });
-  };
+	handleInputChangeFor = (propertyName) => (event) => {
+		this.setState({
+			[propertyName]: event.target.value,
+		});
+	};
 
-  render() {
-    return (
-      <form className="formPanel" onSubmit={this.registerUser}>
-        <h2>Register User</h2>
-        {this.props.store.errors.registrationMessage && (
-          <h3 className="alert" role="alert">
-            {this.props.store.errors.registrationMessage}
-          </h3>
-        )}
-        <div>
-          <label htmlFor="username">
-            Username:
-            <input
-              type="text"
-              name="username"
-              value={this.state.username}
-              required
-              onChange={this.handleInputChangeFor('username')}
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="password">
-            Password:
+	render() {
+		return (
+			<form className='formPanel' onSubmit={this.registerUser}>
+				<h2>Register User</h2>
+				{this.props.store.errors.registrationMessage && (
+					<h3 className='alert' role='alert'>
+						{this.props.store.errors.registrationMessage}
+					</h3>
+				)}
+				<div>
+					<label htmlFor='firstname'>
+						First name:
+						<input
+							type='text'
+							name='firstname'
+							// value={this.state.username}
+							required
+							// onChange={this.handleInputChangeFor('username')}
+						/>
+					</label>
+				</div>
+				<div>
+					<label htmlFor='Lastname'>
+						Last name:
+						<input
+							type='text'
+							name='Lastname'
+							// value={this.state.username}
+							required
+							// onChange={this.handleInputChangeFor('username')}
+						/>
+					</label>
+				</div>
+				<div>
+					<label htmlFor='email'>
+						Email:
+						<input
+							type='text'
+							name='email'
+							// value={this.state.username}
+							required
+							// onChange={this.handleInputChangeFor('username')}
+						/>
+					</label>
+				</div>
+
+				<div>
+					<label htmlFor='password'>
+						Password:
+						<input
+							type='password'
+							name='password'
+							value={this.state.password}
+							required
+							onChange={this.handleInputChangeFor("password")}
+						/>
+					</label>
+				</div>
+				<div>
+					<label htmlFor='Verifypassword'>
+						Verify password:
+						<input
+							type='password'
+							name='verifypassword'
+							// value={this.state.password}
+							required
+							// onChange={this.handleInputChangeFor('password')}
+						/>
+					</label>
+				</div>
+				<div>
+					User social Oauth
+					{/* <label htmlFor="Verifypassword">
+            Verify password:
             <input
               type="password"
-              name="password"
-              value={this.state.password}
+              name="verifypassword"
+              // value={this.state.password}
               required
-              onChange={this.handleInputChangeFor('password')}
+              // onChange={this.handleInputChangeFor('password')}
             />
-          </label>
-        </div>
-        <div>
-          <input className="btn" type="submit" name="submit" value="Register" />
-        </div>
-      </form>
-    );
-  }
+          </label> */}
+				</div>
+				<div>
+					<input className='btn' type='submit' name='submit' value='Register' />
+				</div>
+			</form>
+		);
+	}
 }
 
 export default connect(mapStoreToProps)(RegisterForm);
